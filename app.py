cff53060bc3de0db358e37a3badf874195822bfa
Bolaji Olajide <cooproton@gmail.com>
2020-08-31 03:14:02 +0100

    create UI skeleton

--- app.py
+++ app.py
@@ -1,3 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+def dashboard(request: Request):
+    """
+    displays the stock screener dashboard / homepage
+    """
+    context = {
+        "request": request,
+    }
+    return templates.TemplateResponse("dashboard.html", context)
+
+
+@app.post("/stock")
+def create_stock():
+    """
+    created a stock and stores it in the database
+    """
+    return {
+        "code": "success",
+        "message": "stock created"
+    }
